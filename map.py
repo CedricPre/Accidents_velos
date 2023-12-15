ef1e4f9a00c75d2c4a36eaaa213a9641eb2ac594
pretetc <cedric.pretet@edu.esiee.fr>
2023-12-15 15:46:21 +0100

    avancement map

--- map.py
+++ map.py
@@ -1,13 +1,17 @@
 # import
 import csv
+import json
 import doctest
 import pandas as pd
-import folium, branca
+import folium
+from branca.colormap import LinearColormap
 from folium import Choropleth
 import geopandas as gpd
+import numpy as np
 
 
-def map1(deps : any, lats : any, longs : any, occurences : any) -> None:
+
+def map_accidents_precis(deps : any, lats : any, longs : any, occurences : any) -> None:
     #récupération des départements puis création d'un dashboard
     
     map = folium.Map(location=[48, 2],title = "OpenStreetMap", zoom_start=6)
@@ -35,68 +39,55 @@ def count_zero_coordinates(velo_csv : any) -> int:
 
     return zero_coordinates_count
 
-def map2(velo_csv : any):
-    
-    accidents_par_departement = velo_csv.groupby("dep").size()
+def map_accidents_dep (velo_csv : any, geojson_file : any) -> None:
 
-    map = folium.Map(location=[48.8566, 2.3522],tiles='OpenStreetMap', zoom_start=6)
-
-    for dep, accidents in accidents_par_departement.items():
-        
-        # Obtenir les données de latitude et de longitude pour le département
-        dep_data=  velo_csv[velo_csv["dep"] == dep]
-        lats = dep_data["lat"].str.replace(',', '.').astype(float)
-        longs = dep_data["long"].str.replace(',', '.').astype(float)
-
-        # Vérifier si les valeurs de latitude et longitude sont présentes et non nulles
-        if not lats.isnull().all() and not longs.isnull().all():
-            # Calculer la moyenne des latitudes et des longitudes
-            avg_lat = lats.mean()
-            avg_long = longs.mean()
-
-            # Utiliser le nombre d'accidents pour définir le rayon et la couleur (ajustez selon vos besoins)
-            radius = accidents * 0.010  # Ajustez selon vos besoins
-            color = 'red' if accidents > 10 else 'green'
-
-            folium.CircleMarker(
-                location=(avg_lat, avg_long),
-                radius=radius,
-                color=color,
-                fill=True,
-                fill_color=color
-            ).add_to(map)
-
-    # Enregistrer la carte en HTML
-    map.save(outfile='map_accidents.html')
-
-
-def map3 (velo_csv : any, geojson_file : any) -> None:
-
-
-    # Agréger le nombre d'accidents par département
-    accidents_par_departement = velo_csv.groupby("dep").size().reset_index(name="nombre_accidents")
+    # Agréger les données pour obtenir le nombre d'accidents par département
+    nombre_accidents_par_departement = velo_csv['dep'].value_counts().reset_index()
+    nombre_accidents_par_departement.columns = ['dep', 'nombre_accidents']
 
     # Fusionner les données agrégées avec le GeoJSON
-    geo_data = pd.read_json(geojson_file)
-    merged_data = geo_data.merge(accidents_par_departement, how='left', left_on='properties.code', right_on='dep')
+    geojson_data = pd.read_json(geojson_file)
+
+    geojson_data_flat = pd.json_normalize(geojson_data['features'])
+    merged_data = pd.merge(geojson_data_flat, nombre_accidents_par_departement, left_on='properties.code', right_on='dep', how='left')
+    # Fusionner avec le DataFrame velo_csv pour obtenir les données relatives au sexe
+    merged_data = pd.merge(merged_data, velo_csv[['dep', 'sexe']], left_on='dep', right_on='dep', how='left')
+
+    
+    merged_data['nombre_accidents'].fillna(0, inplace=True)
 
     # Création d'une carte centrée sur la France
     map_center = [46.6031, 1.8883]  # Coordonnées géographiques du centre de la France
     my_map = folium.Map(location=map_center, zoom_start=6)
+
+    # Appliquer le logarithme au nombre d'accidents pour une meilleur visibilité sur la carte, Paris ayant trop d'accident par rapport au autre département
+    merged_data['nombre_accidents_log'] = merged_data['nombre_accidents'].apply(lambda x: 0 if x == 0 else np.log10(x + 1))
+
+    # Calculer la proportion homme-femme par département
+    merged_data['proportion_homme_femme'] = merged_data.groupby('dep')['sexe'].apply(lambda x: (x == '1').sum() / (x == '2').sum() if (x == '2').sum() > 0 else 0)
+
     # Ajouter la choroplèthe avec le nombre d'accidents par département
-    Choropleth(
+    choropleth = folium.Choropleth(
         geo_data=geojson_file,
         name='choropleth',
-        data=velo_csv,
-        columns=['dep', 'nombre_accidents'],
+        data=merged_data,
+        columns=['dep', 'nombre_accidents_log'],
         key_on='feature.properties.code',
-        fill_color='YlGn',
+        fill_color='YlOrRd',
         fill_opacity=0.7,
-        line_opacity=0.2,
-        legend_name="Nombre d\'accidents de vélo par département"
+        line_opacity=0.9,
+        legend_name="Nombre d\'accidents de vélo par département",
+        bins=100
     ).add_to(my_map)
 
-    my_map.save("carte_interactive")
+    my_map.save("carte_interactive.html")
+
+
+
+# Fonction pour afficher des informations au survol d'un département
+def on_each_feature(feature, layer):
+    popup_content = f"Nom du département : {feature['properties']['nom_du_departement']}"  # Remplacez 'nom_du_departement' par le champ que vous avez dans votre GeoJSON
+    layer.add_child(folium.Popup(popup_content))
 
 
 def main():
@@ -106,18 +97,13 @@ def main():
     lats = velo_csv["lat"].str.replace(',','.').astype(float)
     longs = velo_csv["long"].str.replace(',','.').astype(float)
 
-    #on remplace les , par des . et on converti en float pour la map
+    # on remplace les , par des . et on converti en float pour la map
 
     occurences = deps.value_counts()
-    #map1(deps=deps, lats=lats, longs=longs, occurences=occurences)
-
-    c = count_zero_coordinates(velo_csv=velo_csv)
-    #print(c)
-
-    #map2(velo_csv=velo_csv)
+    map_accidents_precis(deps=deps, lats=lats, longs=longs, occurences=occurences)
 
     geojson_file = 'departements.geojson'
-    map3(velo_csv=velo_csv, geojson_file=geojson_file)
+    map_accidents_dep(velo_csv=velo_csv, geojson_file=geojson_file)
     #c
 
 if __name__ == '__main__':
